a744981c63e808440906a9e59a183e456098a842
ShunKOZAWA <crewfanq@gmail.com>
2018-03-31 00:09:19 +0900

    動作確認用サーバを追加

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`running on localhost:${port}`));
